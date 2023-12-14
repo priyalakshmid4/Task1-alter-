cc3a20602abbab6bd3c7a4558c55abc1d550c34c
windows 10 <windows 10@DESKTOP-21ATSCV>
2023-12-14 14:18:25 +0530

    Add project files.

---
+++ Task1(alter)/Controllers/NameController.cs
@@ -0,0 +1,42 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task1_alter_.Controllers
+{
+    public class NameController : Controller
+    {
+        NameEntities db=new NameEntities();
+        // GET: Name
+        [HttpGet]
+        public ActionResult Index(int ? id)
+        {
+            Name name = new Name();
+            return View(name);
+        }
+        [HttpPost]
+        public ActionResult Index(Name name)
+        {
+            try 
+            {
+
+
+                db.Names.Add(name);
+                db.SaveChanges();
+
+                ModelState.Clear();
+                ViewBag.SuccessMessage = "Registeration successfull";
+            }
+            catch (Exception ) 
+             {
+                ViewBag.Message = "Please fill the Form";
+            }
+            return View("Index", new Name());
+             
+            
+        }
+    }
+}
---
+++ Task1(alter)/Models/Namenv.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Task1_alter_.Models
+{
+    public class Namenv
+    {
+        public int Id { get; set; }
+        [Display(Name="Enter the Name")]
+        [Required(AllowEmptyStrings =false,ErrorMessage = "Name is Required")]
+        public string name1 { get; set; }
+        [Display(Name = "Enter the PhoneNo")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "PhoneNo is Required")]
+        public int phoneno { get; set; }
+        [Display(Name = "Enter the age")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "age is Required")]
+        public int age { get; set; }
+    }
+}
